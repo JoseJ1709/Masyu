8bdacc0d9c95393899cd32595811cc27e559e3d6
JoseJ1709 <jaramillojoseandres26@gmail.com>
2024-03-27 15:04:59 -0500

    2.0

--- Masyu/main.py
+++ Masyu/main.py
@@ -20,7 +20,10 @@ class Casilla:
     def conectar_con(self, otra_casilla):
         if otra_casilla not in self.conexiones:
             self.conexiones.append(otra_casilla)
+            self.linea = True
             otra_casilla.conexiones.append(self)
+            otra_casilla.linea = True
+
 
 class Tablero:
     def __init__(self, archivo):
@@ -32,7 +35,73 @@ class Tablero:
             fila, columna, tipo_perla = map(int, linea.split(','))
             self.casillas[fila-1][columna-1].perla = Perla(tipo_perla)
 
+    def reglas_lineas(self,casilla1,casilla2):
+        #revisar que las caillas que quieras conectar esten en la misma fila o columna
+        revisarD = self.casillas[casilla1.fila+1][casilla1.columna]
+        revisarI = self.casillas[casilla1.fila-1][casilla1.columna]
+        revisarA = self.casillas[casilla1.fila][casilla1.columna+1]
+        revisarB = self.casillas[casilla1.fila][casilla1.columna-1]
+        if revisarD == casilla2 or revisarI == casilla2 or revisarA == casilla2 or revisarB == casilla2:
+            print("Se puede conectar lineas check basico")
+            return True
+        else:
+            print("No se puede conectar lineas check basico")
+            return False
+    def reglas_perlas(self,casilla1,casilla2):
+        #revisar si hay dos lineas conectadas en las casillas que se quieren conectar
+        if len(casilla1.conexiones) >= 2 or len(casilla2.conexiones) >= 2:
+            print("No se puede conectar lineas, Mas de dos lineas conectadas")
+            return False
+        #se puede agregar una conexion a las casillas
+        if(casilla1.perla is None and casilla2.perla is None):
+            print("Se puede conectar lineas No hay perlas en las casillas")
+            return True
+        else:
+            #si la casilla tiene una perla negra
+            if casilla1.perla is not None and casilla1.perla.tipo == 2:
+                if(casilla1.columna == casilla2.columna or casilla1.fila == casilla2.fila):
+                    if(casilla1.columna  == casilla2.columna):
+                        revisar1 = self.casillas[casilla1.fila - 1][casilla1.columna]
+                        revisar2 = self.casillas[casilla1.fila + 1][casilla1.columna]
+                        if(revisar1.linea or revisar2.linea):
+                            print("No se puede conectar linea en filas adyacentes, Perla Negra")
+                            return False
+                        else:
+                            print("Se puede conectar linea en filas adyacentes, Perla Negra")
+                            return True
+                    if(casilla1.fila == casilla2.fila):
+                        revisar1 = self.casillas[casilla1.fila][casilla1.columna - 1]
+                        revisar2 = self.casillas[casilla1.fila][casilla1.columna + 1]
+                        if(revisar1.linea or revisar2.linea):
+                            print("No se puede conectar linea en columnas adyacentes, Perla Negra")
+                            return False
+                        else:
+                            print("Se puede conectar linea, Perla Negra")
+                            return True
 
+            #si la casilla tiene perla Blanca
+            if casilla1.perla is not None and casilla1.perla.tipo == 1:
+                if (casilla1.columna == casilla2.columna or casilla1.fila == casilla2.fila):
+                    if (casilla1.columna == casilla2.columna):
+                        revisar1 = self.casillas[casilla1.fila][casilla1.columna+1]
+                        revisar2 = self.casillas[casilla1.fila][casilla1.columna-1]
+                        if (revisar1.linea or revisar2.linea):
+                            print("No se puede conectar linea con filas adyacentes, Perla Blanca")
+                            return False
+                        else:
+                            print("Se puede conectar linea, Perla Blanca ")
+                            return True
+                    if (casilla1.fila == casilla2.fila):
+                        revisar1 = self.casillas[casilla1.fila + 1][casilla1.columna]
+                        revisar2 = self.casillas[casilla1.fila - 1][casilla1.columna]
+                        if (revisar1.linea or revisar2.linea):
+                            print("No se puede conectar linea con columnas adyacentes, Perla Blanca")
+                            return False
+                        else:
+                            print("Se puede conectar linea, Perla Blanca")
+                            return True
+            else:
+                return True
 
 class Aplicacion:
     def __init__(self, master, tablero):
@@ -42,6 +111,8 @@ class Aplicacion:
         self.canvas.pack()
         self.dibujar_tablero()
         self.dibujar_lineas()
+        self.casilla_seleccionada = None
+        self.canvas.bind("<Button-1>", self.manejar_clicks)
 
     def dibujar_tablero(self):
         paso = 500 / self.tablero.tamaño
@@ -56,14 +127,40 @@ class Aplicacion:
                     else:
                         color = 'black'
                     self.canvas.create_oval(x0 + paso*0.25, y0 + paso*0.25, x0 + paso*0.75, y0 + paso*0.75, fill=color)
-
-
     def dibujar_lineas(self):
         paso = 500 / self.tablero.tamaño
         for i in range(self.tablero.tamaño):
             self.canvas.create_line(i * paso, 0, i * paso, 500)
             self.canvas.create_line(0, i * paso, 500, i * paso)
-
+    def manejar_clicks(self, event):
+        fila = int(event.y / (500 / self.tablero.tamaño))
+        columna = int(event.x / (500 / self.tablero.tamaño))
+        casilla_clicada = self.tablero.casillas[fila][columna]
+        if self.casilla_seleccionada is None:
+            self.casilla_seleccionada = casilla_clicada
+        else:
+            if self.tablero.reglas_lineas(self.casilla_seleccionada, casilla_clicada) and self.tablero.reglas_perlas(self.casilla_seleccionada, casilla_clicada):
+                self.casilla_seleccionada.conectar_con(casilla_clicada)
+                self.dibujar_linea(self.casilla_seleccionada, casilla_clicada)
+            else:
+                if(self.casilla_seleccionada.perla != None or casilla_clicada.perla != None):
+                    self.dibujar_perla_roja(self.casilla_seleccionada)
+                    self.dibujar_linea(self.casilla_seleccionada, casilla_clicada)
+                else:
+                    print("No se puede conectar lineas")
+            self.casilla_seleccionada = None
+    def dibujar_linea(self, casilla1, casilla2):
+        paso = 500 / self.tablero.tamaño
+        x1 = casilla1.columna * paso + paso / 2
+        y1 = casilla1.fila * paso + paso / 2
+        x2 = casilla2.columna * paso + paso / 2
+        y2 = casilla2.fila * paso + paso / 2
+        self.canvas.create_line(x1, y1, x2, y2)
+    def dibujar_perla_roja(self, casilla):
+        paso = 500 / self.tablero.tamaño
+        x0 = casilla.columna * paso
+        y0 = casilla.fila * paso
+        self.canvas.create_oval(x0 + paso*0.25, y0 + paso*0.25, x0 + paso*0.75, y0 + paso*0.75, fill='red')
 def main():
     archivo = 'file.txt'
     tablero = Tablero(archivo)
